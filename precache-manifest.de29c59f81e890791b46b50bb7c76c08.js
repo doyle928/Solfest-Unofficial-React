db1186a0328f1cbadb8178c0365755819cd09064
doyle928 <doyle928@yahoo.com>
2023-05-02 11:31:16 -0400

    Updates

---
+++ precache-manifest.de29c59f81e890791b46b50bb7c76c08.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "1795ca8656201a3195c8",
+    "url": "/Solfest-Unofficial-React/static/css/main.aec6701d.chunk.css"
+  },
+  {
+    "revision": "1795ca8656201a3195c8",
+    "url": "/Solfest-Unofficial-React/static/js/main.1795ca86.chunk.js"
+  },
+  {
+    "revision": "bfa17f515437437af62a",
+    "url": "/Solfest-Unofficial-React/static/js/1.bfa17f51.chunk.js"
+  },
+  {
+    "revision": "37b2adbd39a2b826d53e",
+    "url": "/Solfest-Unofficial-React/static/js/2.37b2adbd.chunk.js"
+  },
+  {
+    "revision": "feb2f4ab3b775a6c6b67",
+    "url": "/Solfest-Unofficial-React/static/js/runtime~main.feb2f4ab.js"
+  },
+  {
+    "revision": "6ad6b2e6d9dabe2b4f9247bc4a6ab0de",
+    "url": "/Solfest-Unofficial-React/static/media/lineup_thursday_4x.6ad6b2e6.jpg"
+  },
+  {
+    "revision": "e078cf7604d360e2a1be820dea1b4f75",
+    "url": "/Solfest-Unofficial-React/static/media/logotest.e078cf76.png"
+  },
+  {
+    "revision": "915fab95e4cffc859dbe843714b10f75",
+    "url": "/Solfest-Unofficial-React/static/media/lineup_friday_4x.915fab95.jpg"
+  },
+  {
+    "revision": "5e27428a2a3829e023f56b8415f48089",
+    "url": "/Solfest-Unofficial-React/static/media/lineup_saturday_2x.5e27428a.jpg"
+  },
+  {
+    "revision": "76f8abb42a9f5f1ead5ade36a12ba0a6",
+    "url": "/Solfest-Unofficial-React/static/media/lineup_sunday_4x.76f8abb4.jpg"
+  },
+  {
+    "revision": "011a23d288498e41de9050a50aa1fa71",
+    "url": "/Solfest-Unofficial-React/static/media/map_v2_4x.011a23d2.png"
+  },
+  {
+    "revision": "b2232764041f02a3f4367fdd91c02f7a",
+    "url": "/Solfest-Unofficial-React/static/media/background.b2232764.mp4"
+  },
+  {
+    "revision": "e2bba9cf49b298d6be781c2274694ea3",
+    "url": "/Solfest-Unofficial-React/static/media/MrDafoe-Regular.e2bba9cf.ttf"
+  },
+  {
+    "revision": "1035cee07ce8ec16a4e4f193517f1c4a",
+    "url": "/Solfest-Unofficial-React/static/media/Audiowide-Regular.1035cee0.ttf"
+  },
+  {
+    "revision": "3ade034999153f8110b40724ad74f00e",
+    "url": "/Solfest-Unofficial-React/static/media/Pacifica Condensed Regular.3ade0349.otf"
+  },
+  {
+    "revision": "f34c1b5ca0ee4d9fabdf57591e782403",
+    "url": "/Solfest-Unofficial-React/index.html"
+  }
+];
